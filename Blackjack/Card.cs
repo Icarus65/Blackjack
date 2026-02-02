6e98d2d808ce089eaac552b2e710bd1d5be3438c
Icarus <ndubuisi@okhoro.com>
2026-02-02 18:05:20 +0100

    seperated classes into files

---
+++ Blackjack/Card.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BlackJack.BlackJack;
+
+namespace Blackjack
+{
+    public enum Suit
+    {
+        Hearts,
+        Diamonds,
+        Clubs,
+        Spades
+    }
+    public enum Rank
+    {
+        Two = 2,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack = 10,
+        Queen = 10,
+        King = 10,
+        Ace = 11
+    }
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public Rank Rank { get; set; }
+        public Card(Suit suit, Rank rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+        public override string ToString()
+        {
+            return $"{Rank} of {Suit}";
+        }
+    }
+}
---
+++ Blackjack/Hand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    public class Hand
+    {
+        private List<Card> cards;
+        public Hand()
+        {
+            cards = new List<Card>();
+        }
+        public void AddCard(Card card)
+        {
+            cards.Add(card);
+        }
+        public int GetValue()
+        {
+            int value = cards.Sum(c => (int)c.Rank);
+            int aceCount = cards.Count(c => c.Rank == Rank.Ace);
+            while (value > 21 && aceCount > 0)
+            {
+                value -= 10;
+                aceCount--;
+            }
+            return value;
+        }
+        public override string ToString()
+        {
+            return string.Join(", ", cards);
+        }
+    }
+}
